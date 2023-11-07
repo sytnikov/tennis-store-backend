3f9e7ea18eac3884bb3ac180d227359cc2c2b2a5
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2023-11-07 23:29:50 +0200

    fix productValidation

--- server/controllers/users/deleteUser.ts
+++ server/controllers/users/deleteUser.ts
@@ -3,18 +3,13 @@ import { NextFunction, Request, Response } from "express";
 import usersService from "../../services/usersService";
 import { ApiError } from "../../middlewares/errors/ApiError";
 
-export function deleteUser(
-    req: Request,
-    res: Response,
-    next: NextFunction
-    ) {
-        const id = req.params.userId;
-        const usersData = usersService.getSingleUser(id);
-        if (!usersData) {
-            next(ApiError.resourceNotFound("User can't be deleted"));
-            return;
-        }
-        usersService.deleteUser(id);
-        res.status(200).json({ message: "User deleted" });
+export function deleteUser(req: Request, res: Response, next: NextFunction) {
+  const id = req.params.userId;
+  const usersData = usersService.getSingleUser(id);
+  if (!usersData) {
+    next(ApiError.resourceNotFound("User can't be deleted"));
+    return;
+  }
+  usersService.deleteUser(id);
+  res.status(200).json({ message: "User deleted" });
 }
-
