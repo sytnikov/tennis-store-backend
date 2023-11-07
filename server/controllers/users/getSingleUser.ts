ab11f2e00dbbb13aea88bad3cc357344045074da
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-07 17:22:51 +0200

    add userController

--- server/controllers/users/getSingleUser.ts
+++ server/controllers/users/getSingleUser.ts
@@ -3,16 +3,20 @@ import { Request, Response, NextFunction } from "express";
 import { ApiError } from "../../middlewares/errors/ApiError";
 import usersService from "../../services/usersService";
 
-export function getSingleUser(
+export async function getSingleUser(
     req: Request,
     res: Response,
     next: NextFunction
     ) {
-        const id = Number(req.params.userId);
-        const user = usersService.getSingleUser(id);
-        if (!user) {
-            next(ApiError.resourceNotFound("User not found"));
-            return;
+        try {
+            const id = req.params.userId;
+            const user = await usersService.getSingleUser(id);
+            if (!user) {
+                next(ApiError.resourceNotFound("User not found"));
+                return;
+            }
+            res.status(200).json({ user });
+        } catch (error) {
+            next(ApiError.resourceNotFound("Cannot find single user"));
         }
-        res.status(200).json({ user });
 }
