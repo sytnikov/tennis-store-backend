f7cf4c5c93c2b3648e38bbd69f0f927f1c0245ee
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-07 17:19:47 +0200

    add user services folder

--- server/types/User.ts
+++ server/types/User.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 
 import { userSchema } from "../schemas/userSchema";
+import mongoose from "mongoose";
 
-export type User = z.infer<typeof userSchema>;
+export type User = z.infer<typeof userSchema>& {
+    id: mongoose.Types.ObjectId;
+};;
 
-export type UserUpdate = Omit<Partial<User>, "id">;
+export type UserUpdate = Partial<User>;
