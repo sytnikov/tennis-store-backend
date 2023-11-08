2f8480f4b075f8fde01a89e43aaaa8e487294725
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-08 20:08:51 +0200

    fix emailChecker middleware

--- server/middlewares/emailChecker.ts
+++ server/middlewares/emailChecker.ts
@@ -3,12 +3,6 @@ import UserRepo from "../models/UserModel";
 
 export async function emailChecker(req: Request, res: Response, next: NextFunction) {
     try {
-        const username = await UserRepo.findOne({ name: req.body.name });
-
-        if (username) {
-            return res.status(400).json({ message: "Username already exists" });
-        }
-
         // check if email already exists
         const emailIsExisted = await UserRepo.findOne({ email: req.body.email });
 
