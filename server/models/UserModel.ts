a81b1078610560d7408fd59267e2af45ab74baa1
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-08 17:30:56 +0200

    remove the id from userSchema

--- server/models/UserModel.ts
+++ server/models/UserModel.ts
@@ -1,10 +1,8 @@
 import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
 const UserSchema = new Schema({
-    id: ObjectId,
     name: String,
     email: String,
     password: String,
