1182185bb792dae9cd2fd46e445334171a57bf13
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2023-11-12 11:26:17 +0200

    add validation to add shipment

--- server/controllers/shipments/addShipment.ts
+++ server/controllers/shipments/addShipment.ts
@@ -3,13 +3,16 @@ import { Request, Response, NextFunction } from 'express';
 import shipmentsService from '../../services/shipmentsService';
 import { ApiError } from '../../middlewares/errors/ApiError';
 
-export function addShipment(
+export async function addShipment(
     req: Request,
     res: Response,
     next: NextFunction
     ) {
         try {
-            const shipment = shipmentsService.createShipment(req.body);
+            const shipment = await shipmentsService.createShipment(req.body);
+            if (!shipment || shipment === undefined) {
+                return next(ApiError.resourceNotFound("Cannot add Shipment"));
+            }
             res.status(201).json({shipment, message: "Shipment created"});
         } catch (error) {
             next(ApiError.resourceNotFound("Cannot add Shipment"));
