7c8a083cd3e85d1f6dc8e40b6a5494533fb5d819
Md Shayemur Rahman <mdshayemur.rahman@tuni.fi>
2023-11-21 10:55:47 +0200

    test file configuration

---
+++ server/test/db-helper.ts
@@ -0,0 +1,34 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+import mongoose from 'mongoose';
+
+async function connect() {
+  const mongod = await MongoMemoryServer.create();
+  const uri = await mongod.getUri();
+
+  await mongoose.connect(uri);
+
+  return {
+    closeDatabase: async () => {
+      await mongoose.connection.dropDatabase();
+      await mongoose.connection.close();
+
+      await mongod.stop();
+    },
+    clearDatabase: async () => {
+      const collections = mongoose.connection.collections;
+
+      for (const key in collections) {
+        const collection = collections[key];
+        collection.deleteMany({});
+      }
+    },
+  };
+}
+
+export default connect;
+
+export type MongoHelper = {
+  closeDatabase: () => Promise<void>;
+  clearDatabase: () => Promise<void>;
+};
