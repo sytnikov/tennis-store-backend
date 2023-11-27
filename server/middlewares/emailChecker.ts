d2642f4a2ce5222a57f508f80369a1820de94204
sytnikov <sytnikov.alexey@gmail.com>
2023-11-27 11:26:11 +0200

    initial fullstack branch commit

--- server/middlewares/emailChecker.ts
+++ server/middlewares/emailChecker.ts
@@ -12,6 +12,6 @@ export async function emailChecker(req: Request, res: Response, next: NextFuncti
 
         next();
     } catch (error) {
-        return res.status(500).json({ message: "Something went wrong in emailChecker" });
+        return res.status(500).json({ message: "Something went wrong in email checker" });
     }
 }
