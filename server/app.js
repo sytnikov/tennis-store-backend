18790f4edafafcaa0ad44a1e14353b587027a885
Marc Seitz <marc@mfts.io>
2024-02-05 19:36:14 +1100

    fix: import

--- server/app.js
+++ server/app.js
@@ -13,12 +13,24 @@ import { apiErrorHandler } from "./middlewares/apiErrorHandler";
 import { routeNotFound } from "./middlewares/routeNotFound";
 import { authWithGoogle } from "./middlewares/authWithGoogle";
 
+import mongoose from "mongoose";
+import "dotenv/config";
+
+
 const app = express();
 app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 passport.use(authWithGoogle());
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
 
 app.use("/products", loggingMiddleware, productsRouter);
 app.use("/categories", loggingMiddleware, categoriesRouter);
@@ -29,5 +41,15 @@ app.use("/items", loggingMiddleware, orderItemsRouter);
 
 app.use(apiErrorHandler);
 app.use(routeNotFound);
+
+const port = process.env.PORT || 5000;
+
+const mongoURL = process.env.DB_URL_TENNIS;
+mongoose.connect(mongoURL).then(() => console.log("Connected!"));
+
+app.listen(port, () => {
+  console.log(`👀 Server is running on localhost:${port}`);
+});
+
 
 export default app
